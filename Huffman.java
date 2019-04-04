561a39f0c6e3a9b08df3b168b393318a1db64538
TrimurthuluPosimsetti <40907951+TrimurthuluPosimsetti@users.noreply.github.com>
2019-04-04 20:12:54 +0530

    Update Huffman.java

--- Huffman.java
+++ Huffman.java
@@ -1,3 +1,18 @@
+/*
+Huffman coding assigns variable length codewords to fixed length input characters based on their frequencies. 
+More frequent characters are assigned shorter codewords and less frequent characters are assigned longer codewords. 
+All edges along the path to a character contain a code digit. If they are on the left side of the tree, they will be a 0 (zero). 
+If on the right, they'll be a 1 (one). Only the leaves will contain a letter and its frequency count. 
+All other nodes will contain a null instead of a character, and the count of the frequency of all of it and its descendant characters.
+For instance, consider the string ABRACADABRA. There are a total of  characters in the string. 
+This number should match the count in the ultimately determined root of the tree. Our frequencies are  and . 
+The two smallest frequencies are for  and , both equal to , so we'll create a tree with them. 
+The root node will contain the sum of the counts of its descendants, in this case . 
+The left node will be the first character encountered, , and the right will contain . 
+Next we have  items with a character count of : the tree we just created, the character  and the character . 
+The tree came first, so it will go on the left of our new root node.
+will go on the right. Repeat until the tree is complete, then fill in the 's and 's for the edges. The finished graph looks like:
+*/
 import java.util.*;
  
 abstract class Node implements Comparable<Node> {
@@ -45,16 +60,13 @@ class Decoding {
 */ 
 
 	void decode(String s, Node root) {
-        // System.out.println(root.frequency+" "+root.data+" ");
-        // if(root.left!=null){
-        //     decode(s,root.left);
-        // }
-        // if(root.right!=null){
-        //     decode(s,root.right);
-        // }
         Node temp;
         temp=root;
         for(int i=0;i<s.length();i++){
+	 //if character is equal to '1' i should move to right else move to left side of tree,then i should check for children 
+	 //of that node, if have children to that node ,then based on next character the string i should move left or right 
+	 //('0' means left , '1' means right) if it reaches leaf i should print that node data and pointer must go back to root..
+	 //process will repeat until all leafs visited	
             if(s.charAt(i)=='1'){
                 temp=temp.right;
                 if(i+1<s.length()){
@@ -64,9 +76,9 @@ class Decoding {
                             temp=root;
                             }
                     }
-                    else if(s.charAt(i+1)=='0'){
+                    else if(s.charAt(i+1)=='0'){ 
                         if(temp.left==null){
-                            System.out.print(temp.data);   //1001011
+                            System.out.print(temp.data);   
                             temp=root;
                         }
                     }
